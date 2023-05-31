a649125eba14410356cb6111afef4b95b729d08f
PabloPadilla5 <pablopadillamaldonado@gmail.com>
2023-05-31 12:53:40 -0600

    Fixed the "avoid" parameter in the Directions API URL
    Now it's correctly formatted according to the official documentation: https://developers.google.com/maps/documentation/directions/get-directions#avoid

--- lib/src/network_util.dart
+++ lib/src/network_util.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:http/http.dart' as http;
 
@@ -28,11 +29,19 @@ class NetworkUtil {
       "origin": "${origin.latitude},${origin.longitude}",
       "destination": "${destination.latitude},${destination.longitude}",
       "mode": mode,
-      "avoidHighways": "$avoidHighways",
-      "avoidFerries": "$avoidFerries",
-      "avoidTolls": "$avoidTolls",
       "key": googleApiKey
     };
+    // Build the "avoid" parameter which should be formatted the following way:
+    // avoid=tolls|highways|ferries
+    // See more: https://developers.google.com/maps/documentation/directions/get-directions#avoid
+    String avoid = '${avoidTolls ? 'tolls|' : ''}'
+        '${avoidHighways ? 'highways|' : ''}${avoidFerries ? 'ferries|' : ''}';
+    if (avoid.isNotEmpty) {
+      // Remove the last character of the generated String which should be "|"
+      avoid = avoid.substring(0, avoid.length - 1);
+      params["avoid"] = avoid;
+    }
+
     if (wayPoints.isNotEmpty) {
       List wayPointsArray = [];
       wayPoints.forEach((point) => wayPointsArray.add(point.location));
@@ -45,7 +54,7 @@ class NetworkUtil {
     Uri uri =
         Uri.https("maps.googleapis.com", "maps/api/directions/json", params);
 
-    // print('GOOGLE MAPS URL: ' + url);
+    log('GOOGLE MAPS URL: ' + uri.toString());
     var response = await http.get(uri);
     if (response.statusCode == 200) {
       var parsedJson = json.decode(response.body);
